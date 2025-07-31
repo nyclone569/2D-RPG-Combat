ee1965f0e29012fbb42d3645d92f1a4e6a8addda
nyclone569 <nyclone569@gmail.com>
2025-08-01 04:39:44 +0700

    Add MainMenu Scene with scrolling background using shader graph

---
+++ Assets/Scripts/Scene Management/Menu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.Unity.VisualStudio.Editor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField] private string sceneToLoad;
+    public void Play()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+    public void Quit()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+}
